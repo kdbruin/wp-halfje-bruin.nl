9b15a41de5978fd08b32a6131bbc12d9f1131ae9
Kees de Bruin <halfje.bruin@gmail.com>
2018-08-26 16:31:37 +0200

    Various updates including proper deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# Deploy website to actual hosting environment
+#
+# Any argument passed to the script will be added to the rsync command line.
+
+REMOTE_USER=deb4664
+REMOTE_HOST=halfje-bruin.nl
+REMOTE_DIR=domains/${REMOTE_HOST}/public_html
+
+SSH_KEYFILE=${HOME}/.ssh/antagonist-ssh
+LOCAL_DIR=../wwwroot/
+EXCLUDES=.rsync-excludes
+
+LOGFILE=deploy-"$(date '+%Y%m%d-%H%M%S')".log
+
+rsync "$@"  \
+    --verbose \
+    --archive \
+    --checksum \
+    --chmod=D775,F664 \
+    --no-group \
+    --no-times \
+    --omit-dir-times \
+    --delete-during \
+    --exclude-from=${EXCLUDES} \
+    --log-file=${LOGFILE} \
+    --rsh="ssh -i ${SSH_KEYFILE}" \
+    ${LOCAL_DIR} \
+    ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}
